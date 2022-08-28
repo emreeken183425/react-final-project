5d0b8773bdd77d50e21083f65eeb89d54325e974
emreeken183425 <emreeken1834@gmail.com>
2022-08-28 17:49:53 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <div className='App' >
-      <h2>APP</h2>
+     <Navbar />
+
+     
     </div>
   )
 }
